598e259c8cd14c1314219e51c79323e0ad93abb2
gertibarca <gertinovoberdaliu@gmail.com>
2025-06-04 19:07:39 +0200

    Moduli26

---
+++ Moduli26/js/main.js
@@ -0,0 +1,42 @@
+// console.log("test")
+
+var x = 10;
+var var_value = "Digital School";
+
+var age = 15;
+var message = "Age is " + age;
+
+console.log(message);
+
+var score = 100;
+score = score + 50;
+
+console.log(score);
+
+var a = 4;
+var b = 3;
+var x = a * b;
+
+console.log(x);
+
+var sum = 25 + 75;
+console.log(sum);
+
+var difference = 90 - 40;
+console.log(difference);
+
+var division = 144 / 12;
+console.log(division);
+
+var greeting = "Hello, " + "World!";
+console.log(greeting);
+
+var timesResult = 7 * 8;
+console.log("7 times 8 is " + timesResult);
+
+var count = 5;
+count = count * 2;
+console.log(count);
+
+var expression = "5 * 10";
+console.log("This is a string: " + expression);
