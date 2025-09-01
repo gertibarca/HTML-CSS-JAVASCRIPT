bd98d8a8079081f8f1f029a5d63dc335ccd93e36
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-09-01 19:17:08 +0200

    perseritje

---
+++ Perseritje1/js/main.js
@@ -0,0 +1,31 @@
+var a = 50 + 50;
+console.log(a);
+
+var b = 10;
+var f = 12;
+var results = b * f
+console.log(results)
+
+var n = 99;
+n++;
+console.log(n);
+
+var o=100; 
+n--
+console.log(o);
+
+var v=10;
+var w=11;
+
+console.log( v == w);
+
+var g = 13;
+var z = 13;
+
+console.log(g != z);
+
+
+function showmessage() {
+    alert("Test")
+}
+showmessage();
