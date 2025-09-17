2018ef57fc28b2d5cf7032622e401afce202df54
gertibarca <gertinovoberdaliu@gmail.com>
2025-09-17 19:08:42 +0200

    Moduli35

---
+++ Moduli35/js/main.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    slideIndex += n;
+    showSlides(slideIndex);
+}
+
+setInterval(plusSlides,3000,1);
